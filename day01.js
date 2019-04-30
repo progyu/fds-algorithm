12880f1b467999409d19c885e27fbbaa78626d75
progyu <rocket7101@gmail.com>
2019-04-30 17:00:00 +0900

    es6 applied

--- day01.js
+++ day01.js
@@ -1,25 +1,22 @@
 // 1번 문제
-
 function insensitiveEqual(str1, str2) {
   if (str1.toLowerCase() === str2.toLowerCase()) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 console.log(insensitiveEqual('hello', 'Hello'));
 
 // 2번 문제
 // function leftPad(str, number) {
+//   const differ = number - str.length;
+//   const space = ' ';
+//   const estr = space.repeat(differ) + str;
 //   if (str.length < number) {
-//     var differ = number - str.length;
-//     var space = ' ';
-//     estr = space.repeat(differ) + str;
 //     return estr;
-//   } else {
-//     return str;
 //   }
+//   return str;
 // }
 
 // console.log(leftPad('hello', 8));
@@ -27,11 +24,11 @@ console.log(insensitiveEqual('hello', 'Hello'));
 
 // 3번 문제
 // function searchchar(str) {
-//   var arr = ['a', 'e', 'i', 'o', 'u'];
-//   var strarr = str.split('');
-//   sum = 0;
-//   for (var i = 0; i < arr.length; i++) {
-//     for (var j = 0; j < strarr.length; j++) {
+//   const arr = ['a', 'e', 'i', 'o', 'u'];
+//   const strarr = str.split('');
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < strarr.length; j++) {
 //       if (arr[i] === strarr[j]) {
 //         sum += 1;
 //       }
